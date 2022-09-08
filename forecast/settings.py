8bbde36d65bbaf0fe1c8a6dd8365396314daf7e8
walexhenrique <walex999067@gmail.com>
2022-09-08 19:45:32 -0300

    created html templates and corresponding directories and corrected index.html

--- forecast/settings.py
+++ forecast/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "forecast.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'base_templates',
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,6 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
