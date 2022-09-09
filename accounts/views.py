b1de5b26047aab7cd5589d7494c63a2ded655f71
walexhenrique <walex999067@gmail.com>
2022-09-08 23:03:27 -0300

    Added register view and configured

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import HttpResponse, redirect, render
+from django.core.validators import validate_email
 from slugify import slugify
 
 
@@ -10,7 +11,7 @@ def login(request):
     if request.method != 'POST':
 
         # User logged don't can login again
-        if not auth.get_user(request):
+        if request.user.is_authenticated:
             return redirect('accounts:dashboard')
 
         return render(request, 'accounts/login.html', {'title' : 'Login'})
@@ -29,9 +30,48 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('home:index')
-
+    
 def register(request):
-    return HttpResponse('TEMPORARIO')
+
+    if request.method != 'POST':
+        if request.user.is_authenticated:
+            return redirect('accounts:dashboard')
+
+        return render(request, 'accounts/register.html', {'title': 'Crie a sua conta'})
+    
+    username = request.POST.get('username')
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    email = request.POST.get('email')
+    password_1 = request.POST.get('password_1')
+    password_2 = request.POST.get('password_2')
+
+    if not username or not first_name or not last_name or not email or not password_1 or not password_2:
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+
+    try:
+        validate_email(email)
+    except:
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+
+    if len(username) < 5:
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+    
+    if User.objects.filter(username=username).exists():
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+    
+    if User.objects.filter(email=email).exists():
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+    
+    if len(password_1) < 5 or len(password_2) < 5:
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+    
+    if password_1 != password_2:
+        return render(request, 'accounts/register.html', {'title':'Crie a sua conta'})
+
+    new_user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password_1)
+    new_user.save()
+    return redirect('accounts:login')
 
 @login_required(login_url='accounts:login')
 def dashboard(request):
